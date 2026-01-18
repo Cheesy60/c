d817df1783e8758018b600fd1b6de68249faa319
Cheesy60 <abodwaleed123@gmail.com>
2026-01-19 01:26:43 +0300

    hex to deci or smth done by yours truly

--- try.c
+++ try.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+int htoi(char c[]);
 int main(){
-
-    printf("brooo\n");
- 	printf("bombo");
-    //i have gone too far into min maxxing my workflow that i don't even work anymore
-
-
+   printf("%d",htoi("0x533"));
 }
+int htoi(char c[])
+{
+    int i,n,start;
+    n = 0;
+    start = 0;
+    if(c[0] == '0' && (c[1] == 'x' ||c[1] == 'X'))
+    {start = 2;}
+    for(i=start;(c[i] >= '0' && c[i] <= '9')|| (c[i] >= 'a' && c[i] <= 'f')||(c[i] >= 'A' && c[i] <= 'F');++i)
+    {
+        if(c[i] >= 'a' && c[i] <= 'f')
+        {
+            n = 16 * n + (c[i] - 'a' + 10);
+        }else if (c[i] >= 'A' && c[i] <= 'F') {
+            n = 16 * n + (c[i] - 'A' + 10);
+        }else {
+            n = 16 * n + (c[i] - '0');
+        }
+    }
+    return n;
+}
+
